e9d95f728545f031b71f3404615bc2daa820af2a
Luca Koval <lucakoval@me.com>
2016-03-29 10:30:38 -0400

    Created enemies
    yee

--- Buttons/Buttons/GameScene.swift
+++ Buttons/Buttons/GameScene.swift
@@ -22,3 +22,36 @@ class GameScene: SKScene {
         addChild(player)
     }
 }
+
+func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+}
+
+func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+    return random() * (max - min) + min
+}
+
+func addMonster() {
+    
+    // Create sprite
+    let monster = SKSpriteNode(imageNamed: "monster")
+    
+    // Determine where to spawn the monster along the Y axis
+    let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+    
+    // Position the monster slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+    
+    // Add the monster to the scene
+    addChild(monster)
+    
+    // Determine speed of the monster
+    let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+    
+    // Create the actions
+    let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+    let actionMoveDone = SKAction.removeFromParent()
+    monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+    
+}
