4e697eb8b6eb4a4e17cf9a460b22759d37ac2abf
Luca Koval <lucakoval@me.com>
2016-04-02 17:48:30 -0400

    Created a menu
    yee

---
+++ Buttons/Buttons/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  Buttons
+//
+//  Created by Luca on 4/2/16.
+//  Copyright © 2016 Space!, Ink. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
