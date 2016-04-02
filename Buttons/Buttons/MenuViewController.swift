d54e28c5b831d311de842cd17c57dcc2825119fe
Luca Koval <lucakoval@me.com>
2016-04-02 18:16:23 -0400

    Editing the button
    yee

--- Buttons/Buttons/MenuViewController.swift
+++ Buttons/Buttons/MenuViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SpriteKit
 
 class MenuViewController: UIViewController {
     override func viewDidLoad() {
@@ -37,11 +38,7 @@ class MenuViewController: UIViewController {
         let button = SKShapeNode()
         
         // Created rounded corners and position button
-        let spacingFactor = CGFloat(4.5)/CGFloat(buttonTotal)
-        //let yPos = (((size.height - 10)/(CGFloat(buttonTotal))) + (CGFloat(2 * buttonCount) * ((size.height/(CGFloat(2 * buttonTotal))) - spacingFactor)))
-        let xPos = (((size.width - 10)/(CGFloat(buttonTotal))) + (CGFloat(2 * buttonCount) * ((size.width/(CGFloat(2 * buttonTotal))) - spacingFactor)))
-        //button.path = UIBezierPath(roundedRect: CGRect(x: 5, y: size.height - 5 - yPos, width: size.width - 10, height: ((size.height - 10)/(CGFloat(buttonTotal)) - (spacingFactor * CGFloat(buttonTotal)))), cornerRadius: 4).CGPath
-        button.path = UIBezierPath(roundedRect: CGRect(x: size.width - 5 - xPos, y: 5, width: ((size.width - 10)/(CGFloat(buttonTotal)) - (spacingFactor * CGFloat(buttonTotal))), height: size.height - 10), cornerRadius: 4).CGPath
+        button.path = UIBezierPath(roundedRect: CGRect(x: view.bounds.width/2, y: view.bounds.height/2, width: view.bounds.width/2, height: 20), cornerRadius: 4).CGPath
         
         
         // Coloring and design
