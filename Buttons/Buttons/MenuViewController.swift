750afa3aa7eda81c664bafd3738046996f347484
Luca Koval <lucakoval@me.com>
2016-04-02 18:08:35 -0400

    Creating play button
    yee

--- Buttons/Buttons/MenuViewController.swift
+++ Buttons/Buttons/MenuViewController.swift
@@ -29,5 +29,40 @@ class MenuViewController: UIViewController {
     
     func createPlayButton() {
         
+        // Create colors
+        let customFillColor = UIColor(red: 1, green: 108/255, blue: 0, alpha: 1)
+        let customStrokeColor = UIColor(red: 178/255, green: 75/255, blue: 0, alpha: 1)
+        
+        // Create a button
+        let button = SKShapeNode()
+        
+        // Created rounded corners and position button
+        let spacingFactor = CGFloat(4.5)/CGFloat(buttonTotal)
+        //let yPos = (((size.height - 10)/(CGFloat(buttonTotal))) + (CGFloat(2 * buttonCount) * ((size.height/(CGFloat(2 * buttonTotal))) - spacingFactor)))
+        let xPos = (((size.width - 10)/(CGFloat(buttonTotal))) + (CGFloat(2 * buttonCount) * ((size.width/(CGFloat(2 * buttonTotal))) - spacingFactor)))
+        //button.path = UIBezierPath(roundedRect: CGRect(x: 5, y: size.height - 5 - yPos, width: size.width - 10, height: ((size.height - 10)/(CGFloat(buttonTotal)) - (spacingFactor * CGFloat(buttonTotal)))), cornerRadius: 4).CGPath
+        button.path = UIBezierPath(roundedRect: CGRect(x: size.width - 5 - xPos, y: 5, width: ((size.width - 10)/(CGFloat(buttonTotal)) - (spacingFactor * CGFloat(buttonTotal))), height: size.height - 10), cornerRadius: 4).CGPath
+        
+        
+        // Coloring and design
+        button.fillColor = customFillColor
+        button.strokeColor = customStrokeColor
+        button.lineWidth = 3
+        
+        // Define some properties
+        if CGFloat((buttonCount + 1)) == rand {
+            button.name = "good"
+        } else {
+            button.name = "bad"
+        }
+        print(rand)
+        print(buttonCount + 1)
+        print(button.name)
+        button.userInteractionEnabled = false
+        
+        // Add the button to the array and to the scene
+        shapeNodes.append(button)
+        addChild(button)
+
     }
 }
