96776a7ef4b8363bab1accb7918d0d769cdb23ec
Luca Koval <lucakoval@me.com>
2016-04-05 10:55:49 -0400

    Corners are rounded
    yee

--- Buttons/Buttons/MenuViewController.swift
+++ Buttons/Buttons/MenuViewController.swift
@@ -45,6 +45,7 @@ class MenuViewController: UIViewController {
         //button.path = UIBezierPath(roundedRect: CGRect(x: view.bounds.width/2, y: view.bounds.height/2, width: view.bounds.width/2, height: 20), cornerRadius: 4).CGPath
         //button.frame(roundedRect: CGRectMake(x: view.bounds.width/2, y: view.bounds.height/2, width: view.bounds.width/2, height: 20), cornerRadius: 4).CGPath
         button.frame = CGRectMake(view.bounds.width/4, view.bounds.height/2, view.bounds.width/2, 50)
+        button.layer.cornerRadius = 4
      
         // Coloring and design
         //button.fillColor = customFillColor
