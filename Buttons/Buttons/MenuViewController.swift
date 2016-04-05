6d29d67e6e0d4e826c75aee538b869a743dca7b3
Luca Koval <lucakoval@me.com>
2016-04-05 10:33:04 -0400

    Adding first button
    yee

--- Buttons/Buttons/MenuViewController.swift
+++ Buttons/Buttons/MenuViewController.swift
@@ -28,33 +28,35 @@ class MenuViewController: UIViewController {
         
     }
     
-    /*
+    ///*
     func createPlayButton() {
         
         // Create colors
         let customFillColor = UIColor(red: 1, green: 108/255, blue: 0, alpha: 1)
-        let customStrokeColor = UIColor(red: 178/255, green: 75/255, blue: 0, alpha: 1)
+        //let customStrokeColor = UIColor(red: 178/255, green: 75/255, blue: 0, alpha: 1)
         
         // Create a button
         //let button = SKShapeNode()
         let button = UIButton(type: UIButtonType.System) as UIButton
         
         // Created rounded corners and position button
-        button.path = UIBezierPath(roundedRect: CGRect(x: view.bounds.width/2, y: view.bounds.height/2, width: view.bounds.width/2, height: 20), cornerRadius: 4).CGPath
-        
-        
+        //button.path = UIBezierPath(roundedRect: CGRect(x: view.bounds.width/2, y: view.bounds.height/2, width: view.bounds.width/2, height: 20), cornerRadius: 4).CGPath
+        //button.frame(roundedRect: CGRectMake(x: view.bounds.width/2, y: view.bounds.height/2, width: view.bounds.width/2, height: 20), cornerRadius: 4).CGPath
+        button.frame = CGRectMake(view.bounds.width/2, view.bounds.height/2, view.bounds.width/2, 20)
+     
         // Coloring and design
-        button.fillColor = customFillColor
-        button.strokeColor = customStrokeColor
-        button.lineWidth = 3
+        //button.fillColor = customFillColor
+        button.backgroundColor = customFillColor
+        //button.strokeColor = customStrokeColor
+        //button.lineWidth = 3
         
         // Define some properties
-        button.name = "playButton"
-        button.userInteractionEnabled = false
+        button.setTitle("playButton", forState: UIControlState.Normal)
+        button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
         
         // Add the button to the array and to the scene
         self.view.addSubview(button)
 
     }
-     */
+     //*/
 }
